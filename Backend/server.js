a6b3d97534d3c7970972b5f809dd630ae9df3517
olukinni_029 <olukinnif`150216029@gmail.com>
2025-01-06 18:14:17 +0100

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/paymentDB')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
@@ -104,7 +105,7 @@ app.get('/all-payment',async(req,res)=>{
 })
 
 // Start the server
-const PORT = 3040;
+const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
